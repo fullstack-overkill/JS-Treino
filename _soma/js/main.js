22dbcba0ed67f9336eef6a2b014880178d1ab85a
fullstack-overkill <christophermms.ctt@gmail.com>
2020-07-21 20:54:36 -0300

    Tarefa Soma

---
+++ _soma/js/main.js
@@ -0,0 +1,9 @@
+// Meu conteudo principal de JS
+
+let Numero1 = new Number(prompt("Digite o numero 1"));
+let Numero2 = new Number(prompt("Digite o numero 2"));
+
+let resultado = Numero1 + Numero2;
+
+alert(resultado);
+
